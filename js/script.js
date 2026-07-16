b0ffed88fc45f1105eee8068e221912c7d730bf9
Kiw2008 <65001@phanthong.ac.th>
2026-07-16 14:43:04 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const title = document.querySelector("h1");
+
+title.addEventListener("mouseover",()=>{
+
+title.style.color="#7ee7ff";
+
+});
+
+title.addEventListener("mouseout",()=>{
+
+title.style.color="white";
+
+});
